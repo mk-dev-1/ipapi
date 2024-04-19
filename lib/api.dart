4d6e0cc9ddb9a5d4f18c8cccb3e487d38deb3625
mk-dev-1 <41755310+mk-dev-1@users.noreply.github.com>
2024-04-19 15:29:02 +0200

    Added getRawData method

--- lib/api.dart
+++ lib/api.dart
@@ -7,7 +7,7 @@ import 'models/geo_data.dart';
 /// [IpApi] encapsules functions that query endpoints at IP-API.com,
 /// specifically [getData], which retrieves geo data for a given [ip].
 class IpApi {
-  /// Get geo data from IP-API.com.
+  /// Get geo data from IP-API.com as GeoData object.
   ///
   /// To query a different than the current IP, specify it using [ip]. API
   /// response is provided in English unless a different language is specified
@@ -21,20 +21,37 @@ class IpApi {
     List<String> fields = const [],
     String? apiKey,
   }) async =>
-      get(Uri(
-        scheme: apiKey != null ? 'https' : 'http',
-        host: apiKey != null ? 'pro.ip-api.com' : "ip-api.com",
-        path: '/json/${ip ?? ''}',
-        queryParameters: {
-          if (apiKey != null) 'key': apiKey,
-          if (lang != null) 'lang': lang,
-          if (fields.isNotEmpty) 'fields': fields.join(',')
-        },
-      )).then((response) => response.statusCode == 200
-          ? GeoData.fromJson(jsonDecode(response.body)
-            ..putIfAbsent('query', () => ip)
-            ..addAll({
-              'lang': lang,
-            }))
-          : null);
+      getRawData(
+        ip: ip,
+        lang: lang,
+        fields: fields,
+        apiKey: apiKey,
+      ).then((data) => data != null ? GeoData.fromJson(data) : null);
+
+  /// Get geo data from IP-API.com as json.
+  ///
+  /// To query a different than the current IP, specify it using [ip]. API
+  /// response is provided in English unless a different language is specified
+  /// using [lang]. To receive only specific fields from the API specify them
+  /// using [fields]. Finally, if IP-API.com's pro version should be used,
+  /// specify a valid API key using [apiKey]. Note that the free version uses
+  /// HTTP while the pro version uses HTTPS.
+  static Future<Map<String, dynamic>?> getRawData({
+    String? ip,
+    String? lang,
+    List<String> fields = const [],
+    String? apiKey,
+  }) async {
+    return get(Uri(
+      scheme: apiKey != null ? 'https' : 'http',
+      host: apiKey != null ? 'pro.ip-api.com' : "ip-api.com",
+      path: '/json/${ip ?? ''}',
+      queryParameters: {
+        if (apiKey != null) 'key': apiKey,
+        if (lang != null) 'lang': lang,
+        if (fields.isNotEmpty) 'fields': fields.join(',')
+      },
+    )).then((response) =>
+        response.statusCode == 200 ? jsonDecode(response.body) : null);
+  }
 }
